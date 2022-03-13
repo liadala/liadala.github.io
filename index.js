0ff64b4a291e574f9d57d347b2c63914e024ba1d
Arlena Müller <62029150+liadala@users.noreply.github.com>
2022-03-13 08:43:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,17 @@ function calculatePoints() {
 
     let points = Math.ceil(watching * (selectedTime * 60));
 
-    document.getElementById("result").innerText = `It takes ${selectedTime} Minutes to get ${points} Channelpoints`
+    document.getElementById("result").innerText = `It takes ${secondsToHms(selectedTime*60)} to get ${points} Channelpoints`
+}
+
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour " : " hours ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return `${hDisplay} ${mDisplay} ${sDisplay}`;
 }
